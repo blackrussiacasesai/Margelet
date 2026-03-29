7ea559c8699236c76d7b478c2c5b6b6e95d186a4
blackrussiacasesai <blackrussia.cases@gmail.com>
2026-03-29 18:18:36 +0300

    Delete supabase-migrate-add-file.sql

--- supabase-migrate-add-file.sql
+++
@@ -1,8 +0,0 @@
--- Migration to add file support to messages_app table
--- Run this in Supabase SQL Editor if the file column doesn't exist
-
-ALTER TABLE public.messages_app
-ADD COLUMN IF NOT EXISTS file jsonb;
-
--- Create index for faster queries
-CREATE INDEX IF NOT EXISTS idx_messages_app_file ON public.messages_app USING gin(file);
